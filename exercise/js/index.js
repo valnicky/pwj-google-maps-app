c97c71b5b24e7a06b00f6f228e8d6d2048bf50b6
Valeria <valery0007@gmail.com>
2021-01-21 19:50:18 +0100

    style list

--- exercise/js/index.js
+++ exercise/js/index.js
@@ -1,4 +1,8 @@
  let map;
+ let infoWindow;
+ /*pass:  Database7*/
+ *
+ /
 
  function initMap() {
      let losAngeles = {
@@ -9,15 +13,65 @@
          center: losAngeles,
          zoom: 8
      });
-     createMarker();
+     infoWindow = new google.maps.InfoWindow();
+     getStores();
+
  }
 
- const createMarker = () => {
-     let marker = new google.maps.Marker({
-         position: {
-             lat: 34.063380,
-             lng: -118.358080
-         },
-         map: map
+ const getStores = () => {
+     const API_URL = `http://localhost:3000/api/stores`;
+     fetch(API_URL).then((response) => {
+         if (response.status == 200) {
+             return response.json();
+         } else {
+             throw new Error(response.status);
+         }
+     }).then((data) => {
+         searchLocationsNear(data);
      });
+ }
+
+ const searchLocationsNear = (stores) => {
+     stores.forEach((store, index) => {
+         let latlng = new google.maps.LatLng(
+             store.location.coordinates[1],
+             store.location.coordinates[0]);
+         let name = store.storeName;
+         let address = store.addressLines[0];
+         let phone = store.phoneNumber;
+         let openStatusText = store.openStatusText;
+         bounds.extend(latlng);
+         createMarker(latlng, name, address, openStatusText, phone, index + 1);
+     });
+     map.fitBounds(bounds);
+ }
+
+ const createMarker = (latlng, name, address, openStatusText, phone, storeNumber) => {
+     let html = `
+     <div class="store-info-window">
+     <div class="store-info-name">${name}</div>
+     <div class="store-info-open-status">${openStatusText}</div>
+     <div class="store-info-address">
+     <div class="icon">
+     <i class="fas fa-location-arrow" ></i>
+     </div>
+     <span>${address}</span></div>
+     <div class="store-info-phone">
+     <div class="icon"><i class="fas fa-phone-alt"></i></div>
+     <span><a href="tel:${phone}">${phone}</a>
+     </span>
+     </div>
+     </div>
+     `;
+
+     let marker = new google.maps.Marker({
+         position: latlng,
+         map: map,
+         label: `${storeNumber}`
+     });
+
+     google.maps.event.addEventListener(marker, 'click', function() {
+         infoWindow.setContent(html);
+         infoWindow.open(map, marker);
+     })
  }
